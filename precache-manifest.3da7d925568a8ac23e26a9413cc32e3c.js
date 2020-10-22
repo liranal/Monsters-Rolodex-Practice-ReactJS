3e024ef09cd4710b3dcaaae68762213481d48aea
liranal <liraneilary@gmail.com>
2020-10-22 12:32:05 +0300

    Updates

---
+++ precache-manifest.3da7d925568a8ac23e26a9413cc32e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc174c4283d7da67ec80ccb313bf43c",
+    "url": "/Monsters-Rolodex-Practice-ReactJS/index.html"
+  },
+  {
+    "revision": "bdd79566dab1e0bb9068",
+    "url": "/Monsters-Rolodex-Practice-ReactJS/static/css/main.447ab630.chunk.css"
+  },
+  {
+    "revision": "f174375f3975afe5ea50",
+    "url": "/Monsters-Rolodex-Practice-ReactJS/static/js/2.687dcbb9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Monsters-Rolodex-Practice-ReactJS/static/js/2.687dcbb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd79566dab1e0bb9068",
+    "url": "/Monsters-Rolodex-Practice-ReactJS/static/js/main.a07964fc.chunk.js"
+  },
+  {
+    "revision": "b323dab4bf85d7a142df",
+    "url": "/Monsters-Rolodex-Practice-ReactJS/static/js/runtime-main.43886a83.js"
+  }
+]);
